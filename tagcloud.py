4010a9e27d7f2e44596e7fa708e18a8452acc02a
luciano nb <luciano.buglioni@gmail.com>
2023-10-30 16:19:03 -0300

    quenado

--- tagcloud.py
+++ tagcloud.py
@@ -6,6 +6,7 @@ l = 0;
 # with open('index.html') as topo_file:
 	# for line in topo_file:
 file = open('index.html','r')
+outfile = open('test.html','w')
 lines = file.readlines()
 # for line in file.readlines():
 main_end = False;
@@ -18,9 +19,14 @@ while (l < len (lines)):
 			if ("</li>" in lines[l]):
 				end = True;
 			if ("#" in lines[l]):
-				taglines+=1;
+        # tagl=lines[l];
+        # taglist = (lines[l]).split("#")
+				taglines += 1;
 				print('found tagline in line %d' %l);
 				print('Tags %s\n',lines[l])
+                # print('Tag count %d\n',len(taglist));
+                # for t in range(0,len(taglist):
+                
 	l +=1
 	print("line %d\n" %l);
 	# if ('</main>' in lines[l]):
